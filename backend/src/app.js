22062b619160fe261faad16f78645859012b8b33
Kris-052005 <Kralvaradogu@est.utn.ac.cr>
2026-06-16 09:50:13 -0600

    Se hicieron cambios al crear variables de entorno y leerlas

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
-const app = express(); 
+import dotenv from "dotenv"; 
+
+dotenv.config();
+const NAME=process.env.SERVER_NAME;
+const VERSION=process.env.SERVER_VERSION;
+const DESCRIPTION=process.env.SERVER_DESCRIPTION;
+const PORT=process.env.SERVER_PORT;
+
+const app = express();
 app.get("/", (req, res) => {
-    res.send("<h1>Funcionando</h1>");
+  res.send(`<h1>${NAME}</h1> <p>${VERSION}</p><p>${DESCRIPTION}</p>`);
 });
 app.listen(4000, () => {
-    console.log("Servidor corriendo");
+  console.log(`${NAME} Corriendo en el puerto ${PORT}`);
 });
+
